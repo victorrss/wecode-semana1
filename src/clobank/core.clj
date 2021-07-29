ae335e558ffeec4776d28cddf84299c88d2cbf93
Victor Rubens <victor.rubens@nubank.com.br>
2021-07-29 12:14:19 -0300

    basic

--- src/clobank/core.clj
+++ src/clobank/core.clj
@@ -1,1 +1,8 @@
-(ns clobank.core)
+(ns clobank.core
+  (:require [clobank.logic :as cb.logic]))
+
+(println "\nTransações por cliente e categoria: ")
+(cb.logic/transactions-by-customer-category)
+
+(println "\nTransações por categoria: ")
+(cb.logic/transactions-by-category)
---
+++ src/clobank/logic.clj
@@ -0,0 +1,55 @@
+(ns clobank.logic
+  (:require [clobank.db :as cb.db]))
+
+(defn get-total [v]
+  (->> v
+       (map #(get % :amount))
+       (reduce +)
+       ))
+
+(defn transform-group-with-sum
+  [[k, v]]
+  {
+   k (get-total v)
+   })
+
+(defn group-total-by-category
+  [ccs]
+  (->> ccs
+       (map :transactions)
+       (reduce into [])
+       (group-by :category)
+       (map transform-group-with-sum)
+       ))
+
+(defn get-amount-of-transactions-customer-by-category
+  [customer]
+  (->> customer
+       (map :credit-cards)
+       (map group-total-by-category)
+       ))
+
+(defn group-transactions-by-customer
+  [[guid customer]]
+  {
+   :customer    guid,
+   :by-category (get-amount-of-transactions-customer-by-category customer)
+   })
+
+(defn get-all-credit-cards [customer]
+  (->> customer
+       :credit-cards
+       first))
+
+(defn transactions-by-category []
+  (->> (cb.db/clobank-database)
+       (map get-all-credit-cards)
+       (group-total-by-category)
+       println
+       ))
+
+(defn transactions-by-customer-category []
+  (->> (cb.db/clobank-database)
+       (group-by :guid)
+       (map group-transactions-by-customer)
+       println))
